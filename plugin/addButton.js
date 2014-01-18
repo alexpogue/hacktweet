cecfe79a8ea11fd3835f6eb2bd7e147d6c506286
Alex Pogue <alex@epogue.com>
2014-01-18 00:12:00 -0600

    Created 'Pin' button

--- plugin/addButton.js
+++ plugin/addButton.js
@@ -1,11 +1,12 @@
-replaceContentInContainer("tweet", "This tweet has been replaced! >:)");
+replaceContentInContainer('more-tweet-actions', '<a role="button" class="with-icn favorite js-tooltip" href="#"><span class="icon sm-fav"></span><b>Pin</b></a>');
 
 function replaceContentInContainer(matchClass,content) {
     var elems = document.getElementsByTagName('*'), i;
     for (i in elems) {
         if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
                 > -1) {
-            elems[i].innerHTML = content;
+            elems[i].innerHTML += content;
+            /*elems[i].insertAdjacentHTML('beforeBegin', content);*/
         }
     }
 }
